e92606f4593ac5aff4ed9b8bb30231a987103cae
Андрей Боронников <boronnikov_a_i@mail.ru>
2021-09-15 22:53:50 +0300

    Create EcsStartup and project structure

---
+++ FlappyCube/Assets/Scripts/EcsStartup.cs
@@ -0,0 +1,37 @@
+using Leopotam.Ecs;
+using UnityEngine;
+
+sealed class EcsStartup : MonoBehaviour
+{
+	private EcsWorld _world;
+	private EcsSystems _systems;
+
+	private void Start()
+	{
+		_world = new EcsWorld();
+		_systems = new EcsSystems(_world);
+		
+#if UNITY_EDITOR
+		Leopotam.Ecs.UnityIntegration.EcsWorldObserver.Create(_world);
+		Leopotam.Ecs.UnityIntegration.EcsSystemsObserver.Create(_systems);
+#endif
+		_systems
+			.Init();
+	}
+
+	private void Update()
+	{
+		_systems?.Run();
+	}
+
+	private void OnDestroy()
+	{
+		if (_systems != null)
+		{
+			_systems.Destroy();
+			_systems = null;
+			_world.Destroy();
+			_world = null;
+		}
+	}
+}
